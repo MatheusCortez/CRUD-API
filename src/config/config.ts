2bdebbcb546276d0440f50683d64405e3d2b038e
“Matheus <matheus.cortez@live.com>
2022-02-21 15:00:35 -0300

    linter

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,10 @@
 require('dotenv').config();
 
-
 export const Config = {
-    DataBase: {
-        DB_CONFIG: process.env.DATABASE,
-
-    },
-    APICEP: {
-        CEP_URL: process.env.API_VIACEP,
-    }
-}
+  DataBase: {
+    DB_CONFIG: process.env.DATABASE,
+  },
+  APICEP: {
+    CEP_URL: process.env.API_VIACEP,
+  },
+};
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,8 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
 import { Address } from './../../types/Adress.type';
 
 export class User {
-    id: string;
-    name: string;
-    email: string;
-    address?: Address;
+  id: string;
+  name: string;
+  email: string;
+  address?: Address;
 }
